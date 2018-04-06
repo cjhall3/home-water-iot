00b94926038af66f37ece7267d2749499753b7b7
Connor Hall <cjhall3@ncsu.edu>
2018-04-05 21:20:55 -0400

    Added sample for reading sensor data

---
+++ samples/flowTest.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var b = require("bonescript");
+var input = "P9_19";
+var duration = 10000;
+var pulseCount = 0;
+var attached = false;
+
+console.log("Please connect the digital sensor output to " + input);
+
+b.pinMode(input, b.INPUT) || die("Failed to set pin mode");
+b.attachInterrupt(input, true, b.RISING, handler) || die("Failed to attached interrupt");
+setTimeout(detach, duration);
+
+function handler(val) {
+    if (val.attached) {
+        attached = true;
+        console.log("Attached!");
+    } else if(attached) {
+        console.log("PING " + ++pulseCount);
+    } else {
+        die("Handler did not attach properly");
+    }
+}
+
+function detach() {
+    if (attached) {
+        attached = false;
+        console.log("Detaching handler");
+        b.detachInterrupt(input);
+    }
+}
+
+function die(msg) {
+    detach();
+    process.exit(1);
+}
--- samples/test.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-b = require("bonescript");
-assert b;
