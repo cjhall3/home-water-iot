ccf7445247fc1b8eab9a9e941888393f7e91bf4f
Caleb Scott <cwscott3@ncsu.edu>
2018-04-19 14:34:23 -0400

    Updated server code

--- samples/final_server.js
+++ samples/final_server.js
@@ -15,8 +15,8 @@ var flow_rate          = 0.0;
 var flow_mL            = 0;
 var total_mL           = 0;
 var attached           = false;
-var FLOW_MAX_NORMAL    = 2000;
-var FLOW_MAX_CRITICAL  = 4000;
+var FLOW_MAX           = 2000;
+var flow_max           = FLOW_MAX;
 console.log( "   FLOW PIN: " + flow_pin );
 
 // Ultrasonic sensor pinout
@@ -39,8 +39,9 @@ var faucet_B_client = null;
 var faucet_A_usage = 0;
 var faucet_B_usage = 0;
 
-// Conservative mode
+// State of the water supply
 var conservative_mode = false;
+var is_empty = false;
 
 // -------------------------------MAIN CODE------------------------------
 
@@ -60,6 +61,7 @@ var server = net.createServer( function( c ) {
             total_mL -= parsed_read;
         }
 
+        // Assign faucets
         if( faucet_id === "A" && faucet_A_client == null ) {
             console.log( "Faucet A connected..." );
             faucet_A_client = c;
@@ -76,31 +78,18 @@ var server = net.createServer( function( c ) {
                 faucet_B_client = null;
             });
         }
-        if( conservative_mode ) {
-            if( faucet_id === "A" && faucet_flow_read >= FLOW_MAX_NORMAL ) {
-                console.log( "Stopping faucet A..." );
-                faucet_A_client.write( "STOP" );
-            }
-            else if( faucet_id === "B" && faucet_flow_read >= FLOW_MAX_NORMAL ) {
-                console.log( "Stopping faucet B..." );
-                faucet_B_client.write( "STOP" );
-            }
-            else {
-                c.write( "OK" );
-            }
+
+        // Check faucet reading with the maximum threshold for water use
+        if( faucet_id === "A" && faucet_flow_read >= flow_max ) {
+            console.log( "Stopping faucet A..." );
+            faucet_A_client.write( "STOP" );
+        }
+        else if( faucet_id === "B" && faucet_flow_read >= flow_max ) {
+            console.log( "Stopping faucet B..." );
+            faucet_B_client.write( "STOP" );
         }
         else {
-            if( faucet_id === "A" & faucet_flow_read >= FLOW_MAX_CRITICAL ) {
-                console.log( "Faucet A has used all its reserved water, stopping..." );
-                faucet_A_client.write( "STOP" );
-            }
-            else if( faucet_id === "B" & faucet_flow_read >= FLOW_MAX_CRITICAL ) {
-                console.log( "Faucet B has used all its reserved water, stopping..." );
-                faucet_A_client.write( "STOP" );
-            }
-            else {
-                c.write( "OK" );
-            }
+            c.write( "OK" );
         }
     });
 });
@@ -115,62 +104,71 @@ b.attachInterrupt( flow_pin, true, b.FALLING, pulseCounter ) || die( "[ERROR] Fa
 setInterval( loop, 1000 );
 
 setInterval( resetQuota, 1000 * 60 );
+
 // --------------------------FUNCTION DECLARATIONS-----------------------
 
 function resetQuota() {
-  // TODO: send RESET signal to clients
-  console.log( "Quota for water usage expired. Resetting..." );
-  if( faucet_A_client != null ) {
-    faucet_A_client.write( "RESET" );
-  }
-  if( faucet_B_client != null ) {
-    faucet_B_client.write( "RESET" );
-  }
+    console.log( "Quota for water usage expired. Resetting..." );
+    if( faucet_A_client != null ) {
+        faucet_A_client.write( "RESET" );
+    }
+    if( faucet_B_client != null ) {
+        faucet_B_client.write( "RESET" );
+    }
 }
 
 function loop() {
-  b.detachInterrupt( flow_pin );
-  flow_rate = pulse_count / calibration_factor;
-  flow_mL = ( flow_rate / 60 )*1000;
-  total_mL += flow_mL;
+    b.detachInterrupt( flow_pin );
+    flow_rate = pulse_count / calibration_factor;
+    flow_mL = ( flow_rate / 60 )*1000;
+    total_mL += flow_mL;
+    
+    b.analogRead( ultrasonic_pin, updateWaterLevel );
 
-  b.analogRead( ultrasonic_pin, updateWaterLevel );
-
-  // THIS CALCULATION IS IMPORTANT:
-  // We use the ultrasonic sensor to get a precise reading of where the water level is,
-  // with the use of the flow sensors and the ultrasonic sensor, we can use "analytics" to 
-  // adjust for "drift" in the error of the flow sensor alone.
-  if( ultrasonic_distance < critical_level && flow_rate == 0 ) {
-    conservative_mode = true;
-  }
-  else {
-    conservative_mode = false;
-  }
-
-  pulseCount = 0;
-  b.attachInterrupt( flow_pin, true, b.FALLING, pulseCounter ) || die( "[ERROR] Failed to set flow_pin handler..." );
+    // Assess the water level, and change state depending on it
+    if( ultrasonic_distance <= bottom_level ) {
+        // Shut down the system, until the water level fills past the critical level
+        is_empty = true;
+    }
+    else if( ultrasonic_distance <= critical_level ) {
+        // Go into "conservative mode" if no water is coming into the system
+        // Otherwise, continue as normal
+        if( flow_rate == 0 ) {
+            conservative_mode = true;
+            flow_max = ( total_mL - 500 ) / 2;
+        }
+    }
+    else {
+        // Proceed as normal
+        conservative_mode = false;
+        is_empty = false;
+        flow_max = FLOW_MAX;
+    }
+    
+    pulseCount = 0;
+    b.attachInterrupt( flow_pin, true, b.FALLING, pulseCounter ) || die( "[ERROR] Failed to set flow_pin handler..." );
 }
 
 function updateWaterLevel( voltage ) {
-  ultrasonic_distance = voltage * ultrasonic_distance_factor;
+    ultrasonic_distance = voltage * ultrasonic_distance_factor;
 }
 
 function pulseCounter( val ){
-  if ( val.attached ) {
-    attached = true;
-  } else if( attached ) {
-      ++pulseCount;
-  } else {
-      die("[ERROR] Flow sensor handler did not attach...");
-  }
+    if ( val.attached ) {
+        attached = true;
+    } else if( attached ) {
+        ++pulseCount;
+    } else {
+        die("[ERROR] Flow sensor handler did not attach...");
+    }
 }
 
 function detach() {
-  if ( attached ) {
-    attached = false;
-    console.log( "Detaching flow sensor handler..." );
-    b.detachInterrupt( flow_pin );
-  }
+    if ( attached ) {
+        attached = false;
+        console.log( "Detaching flow sensor handler..." );
+        b.detachInterrupt( flow_pin );
+    }
 }
 
 function die( msg ) {
