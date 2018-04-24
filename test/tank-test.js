c7cdcb55e75afb7e0e7b09b02fc259e925a84528
Connor Hall <cjhall3@ncsu.edu>
2018-04-24 23:50:15 +0000

    Updated tank tests

--- test/tank-test.js
+++ test/tank-test.js
@@ -1,4 +1,5 @@
 var utils = require("./test-utils");
+var config = require("../lib/utils/config");
 var Server = require("../lib/net/server");
 
 var ask = utils.ask;
@@ -10,54 +11,62 @@ var run = function(tank) {
 
     return Promise.resolve()
         .then(() => ask("Running tank tests. Please run faucet tests first. Continue?"))
-        .then(() => tank.readLevel())
-        .then(level => print("Proximity sensor measured " + level + " liters"))
-        .then(() => ask("Is this measurement accurate?"))
+        .then(() => tank.currentWaterLevel())
+        .then(level => print("Proximity sensor measured " + level + " centimeters"))
+        .then(() => tank.currentVolume())
+        .then(vol => print("Tank has " + vol + " mL"))
+        .then(() => ask("Are these measurements accurate?"))
         .then(() => ask("Please fill the input tank. Ready?"))
-        .then(() => tank.resetWater())
+        .then(() => tank._totalWaterInput = 0)
         .then(() => ask("Open the manual input valve and wait for the water to stop. Finished?"))
-        .then(() => print("Measured " + tank.waterUsage + " liters of flow"))
+        .then(() => print("Measured " + tank._totalWaterInput + " mL of flow"))
         .then(() => ask("Is this measurement accurate?"))
-        .then(() => tank.readLevel())
-        .then(level => print("Proximity sensor measured " + level + " liters"))
+        .then(() => tank.currentWaterLevel())
+        .then(level => print("Proximity sensor measured " + level + " centimeters"))
+        .then(() => tank.currentVolume())
+        .then(vol => print("Tank has " + vol + " mL"))
         .then(() => ask("Is this measurement accurate?"))
         .then(() => config.getSystemConfig())
-        .then(config => {
+        .then(conf => {
             print("Starting server...");
-            server = new Server(config, tank);
+            server = new Server(conf, tank);
             return server.start();
         })
-        .then(() => {
+        .then(() => ask("How many faucets are you using?", "input"))
+        .then(num => {
             print("Please press enter on both faucet beaglebones");
             print("Wating for connections...");
-            var connections = 0;
             return new Promise(accept => {
+                var connections = 0;
                 server._server.on("connection", () => {
                     connections++;
-                    if (connections === 2) {
+                    console.log(connections, num);
+                    if (connections === +num) {
                         accept();
                     }
                 });
-            }
+            });
         })
         .then(() => {
             print("Connections successful");
             print("Opening valves...");
-            // TODO: tell faucets to open
+            // Tank will automatically tell them to open
         })
-        .then(() => wait(2000))
+        .then(() => wait(3000))
         .then(() => {
             print("Closing valves...");
-            // TODO: tell faucets to close
+            return tank._sendQuotas(0, false);
         })
         .then(() => wait(5000))
         .then(() => ask("Did water exit both faucets?"))
         .then(() => {
-            // TODO: ask faucets for water usage
-            print("Measured ...");
-            print("Measured ...");
+            tank._users.forEach(u => 
+                print("Measured " + u.waterUsage + " mL for user " + u.socket.address().address)
+            );
         })
-        .then(() => ask("Are these measurements accurate?"));
+        .then(() => ask("Are these measurements accurate?"))
+        .then(() => ask("You may now end the faucet tests, then press enter"))
+        .then(() => server.stop());
 };
 
 module.exports = run;
