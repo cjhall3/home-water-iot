57b1a0a5e22453e2ea2310e8f74c77d2c7da409d
cwscott3 <cwscott3@ncsu.edu>
2018-04-26 17:37:11 -0400

    fixed up specialized tcp buffer server

--- samples/final_server_mod.js
+++ samples/final_server_mod.js
@@ -18,7 +18,7 @@ var attached           = false;
 var FLOW_MAX_NORMAL    = 2000;
 var flow_max           = FLOW_MAX_NORMAL;
 var critical_level     = 9000;
-var empty_level        = 2000;
+var empty_level        = 2500;
 console.log( "   FLOW PIN: " + flow_pin );
 
 // Ultrasonic sensor pinout
@@ -26,9 +26,6 @@ var ultrasonic_pin                = "P9_40";
 var ultrasonic_distance           = 0;
 var ultrasonic_distance_slope     = -636.02;
 var ultrasonic_distance_intercept = 41.94;
-var ultrasonic_max_level          = 25.01;
-var ultrasonic_critical_level     = 16.11;
-var ultrasonic_empty_level        = 3.14;
 console.log( "   ULTRASONIC PIN: " + ultrasonic_pin );
 
 // Server command strings
@@ -68,7 +65,7 @@ var server = net.createServer( function( c ) {
         }
 
         // Now parse the message, then remove it from the buffer
-        var response = buf.substring( 0, d_index );
+        var response = buf.substring( 0, d_index ).split( "," );
         buf = buf.substring( d_index + 1 );
 	var faucet_id = response[ 0 ];
 	var faucet_flow_read = response[ 1 ];
@@ -99,8 +96,7 @@ var server = net.createServer( function( c ) {
 
         // Check faucet reading with the maximum threshold for water use
         if( faucet_id === "A" ) {
-	    faucet_A_usage += parsed_read;
-	    total_mL -= faucet_A_usage;
+	    faucet_A_usage = parsed_read;
 	    if( faucet_A_usage >= flow_max ) {
 		faucet_A_client.write( "STOP;" );
 	    }
@@ -109,8 +105,7 @@ var server = net.createServer( function( c ) {
 	    }
         }
 	else if( faucet_id === "B" ) {
-	    faucet_B_usage += parsed_read;
-	    total_mL -= faucet_B_usage;
+	    faucet_B_usage = parsed_read;
 	    if( faucet_B_usage >= flow_max ) {
 		faucet_B_client.write( "STOP;" );
 	    }
@@ -176,13 +171,35 @@ function loop() {
         if( !is_critical && is_normal ) {
             faucet_A_usage = 0;
             faucet_B_usage = 0;
+	    if( faucet_A_client != null ) {
+	        faucet_A_client.write( "RESET;" );
+	    }
+	    if( faucet_B_client != null ) {
+	        faucet_B_client.write( "RESET;" );
+	    }
+	    is_critical = true;
         }
-        is_normal = false;
-        is_critical = true;
-        is_empty = false;
-        flow_max = ( critical_level - empty_level ) / 2;
+	else if( !is_critical && is_empty ) {
+	    // Do nothing, we need to wait to refill the bucket
+	}
+	else {
+	    is_normal = false;
+            is_critical = true;
+            is_empty = false;
+            flow_max = ( critical_level - empty_level ) / 2;
+	}
     }
     else {
+	if( (!is_normal && is_critical) || (!is_normal && is_empty) ) {
+	    faucet_A_usage = 0;
+	    faucet_B_usage = 0;
+	    if( faucet_A_client != null ) {
+	        faucet_A_client.write( "RESET;" );
+	    }
+	    if( faucet_B_client != null ) {
+	        faucet_B_client.write( "RESET;" );
+	    }
+	}
         is_normal = true;
         is_critical = false;
         is_empty = false;
@@ -202,7 +219,10 @@ function updateWaterLevel( voltage ) {
 
     // Calculate volume, and compare to the flow sensor readings
     var volume = Math.PI * Math.pow(14.25, 2) * ultrasonic_distance;
-    console.log( "Calculated volume: " + parseFloat( volume ).toFixed( 3 ) + " mL" );
+    if( volume < 0 ) {
+	volume = total_mL;
+    }
+    total_mL = (total_mL + volume) / 2;
 }
 
 function pulseCounter( val ){
